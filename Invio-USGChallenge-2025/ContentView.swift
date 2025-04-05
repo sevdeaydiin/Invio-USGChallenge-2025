8fb7e2c47d905be3dd6dc1d3e392d8c5a4566a54
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2025-04-05 22:07:06 +0300

    Initial Commit

---
+++ Invio-USGChallenge-2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Invio-USGChallenge-2025
+//
+//  Created by Sevde Aydın on 4/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
