e9c40848e1b02c0a1750a158be4100213b69ca7f
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2025-04-16 20:04:01 +0300

    feat(core): Create LocationManager for user location permission

---
+++ Invio-USGChallenge-2025/Core/Managers/LocationManager.swift
@@ -0,0 +1,28 @@
+//
+//  LocationManager.swift
+//  Invio-USGChallenge-2025
+//
+//  Created by Sevde Aydın on 4/16/25.
+//
+
+import CoreLocation
+import SwiftUI
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    
+    @Published var userLocation: CLLocationCoordinate2D?
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        userLocation = location.coordinate
+    }
+}
