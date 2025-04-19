8baa7fc307a21aa9a5213518405822298b16ab5c
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2025-04-19 21:44:11 +0300

    feat(feature): Refactor CityMapView with user location tracking, toolbar, and permission handling

--- Invio-USGChallenge-2025/Features/CityMap/CityMapView.swift
+++ Invio-USGChallenge-2025/Features/CityMap/CityMapView.swift
@@ -10,50 +10,114 @@ import MapKit
 
 struct CityMapView: View {
     @ObservedObject var viewModel: CityMapViewModel
+    @State private var mapRegion: MKCoordinateRegion
+    @Environment(\.dismiss) var dismiss
+    
+    init(viewModel: CityMapViewModel, mapRegion: MKCoordinateRegion? = nil) {
+        self.viewModel = viewModel
+        if let region = mapRegion {
+            self._mapRegion = State(initialValue: region)
+        } else {
+            self._mapRegion = State(initialValue: MKCoordinateRegion())
+        }
+    }
     
     var body: some View {
-        VStack {
-            Map(coordinateRegion: .constant(MKCoordinateRegion(
-                center: CLLocationCoordinate2D(latitude: viewModel.locations.first?.coordinates.lat ?? 0,
-                                               longitude: viewModel.locations.first?.coordinates.lng ?? 0),
-                span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-            )), annotationItems: viewModel.locations) { location in
-                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.coordinates.lat, longitude: location.coordinates.lng),
-                          tint: .blue)
-            }
-            .edgesIgnoringSafeArea(.all)
-            .navigationTitle(viewModel.cityName)
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 15) {
-                    ForEach(viewModel.locations) { location in
-                        VStack {
-                            if let imageUrl = location.image, let url = URL(string: imageUrl) {
-                                AsyncImage(url: url) { image in
-                                    image.resizable()
-                                } placeholder: {
-                                    ProgressView()
-                                }
-                                .frame(width: 100, height: 100)
-                                .cornerRadius(10)
-                            }
-                            Text(location.name)
-                                .font(.headline)
-                            Button("Detaya Git") {
-                                // Navigate to detail view
-                            }
-                            .buttonStyle(.borderedProminent)
+        ZStack(alignment: .bottom) {
+            Map(coordinateRegion: $mapRegion, annotationItems: viewModel.locations + [Location(id: -1, name: "Kullanıcı Konumu", description: "", coordinates: Coordinates(lat: viewModel.userLocation?.latitude ?? 0, lng: viewModel.userLocation?.longitude ?? 0), image: nil)]) { location in
+                MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: location.coordinates.lat, longitude: location.coordinates.lng)) {
+                    if location.id == -1 {
+                        if viewModel.userLocation != nil {
+                            // User location
+                            Image(systemName: "scope")
+                                .font(.title2)
+                                .foregroundStyle(.blue)
+                                .padding(3)
+                                .background(Circle().fill(Color.white).shadow(radius: 5))
                         }
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.white).shadow(radius: 5))
+                    } else {
+                        // City locations
+                        Image(systemName: "mappin.circle.fill")
+                            .foregroundStyle(.red)
+                            .font(.title)
                     }
                 }
-                .padding()
             }
+            .ignoresSafeArea(.all)
+            
+            .onAppear { // Set initial map region to first location
+                mapRegion = MKCoordinateRegion(
+                    center: CLLocationCoordinate2D(latitude: viewModel.locations.first?.coordinates.lat ?? 0,
+                                                   longitude: viewModel.locations.first?.coordinates.lng ?? 0),
+                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+                viewModel.onAppear()
+            }
+            
+            VStack(alignment: .trailing) {
+                // Current location
+                LocationButton {
+                    Task { @MainActor in
+                        if viewModel.handleLocationButtonTap() {
+                            if let userLocation = viewModel.userLocation {
+                                mapRegion.center = userLocation
+                                mapRegion.span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                            }
+                        }
+                    }
+                }
+                
+                // Location cards
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 15) {
+                        ForEach(viewModel.locations) { location in
+                            HorizontalLocationCard(action: {
+                                
+                            }, location: location)
+                        }
+                    }.padding()
+                }
+            }
+        }
+        .alert("Konum İzni", isPresented: $viewModel.showLocationPermissionAlert) {
+            Button("Evet") {
+                viewModel.requestLocationPermission()
+            }
+            Button("Hayır", role: .cancel) { }
+        } message: {
+            Text("Kendi konumunu haritada görmek ister misin?")
+        }
+        .alert("Konum İzni Gerekli", isPresented: $viewModel.shouldShowSettingsAlert) {
+            Button("Ayarlara Git") {
+                viewModel.openSettings()
+            }
+            Button("İptal", role: .cancel) { }
+        } message: {
+            Text("Konumunuzu görebilmek için ayarlardan konum iznini etkinleştirmeniz gerekmektedir.")
+        }
+        .toolbar {
+            ToolbarItemView(cityName: viewModel.cityName)
         }
     }
 }
 
-#Preview {
-    CityMapView(viewModel: CityMapViewModel(cityName: "Ankara", locations: CityResponse.mockData.data.first?.locations ?? []))
+private struct ToolbarItemView: ToolbarContent {
+    @Environment(\.dismiss) var dismiss
+    let cityName: String
+    
+    var body: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarLeading) {
+            Image(systemName: "chevron.left")
+                .foregroundColor(.black)
+                .onTapGesture {
+                    dismiss()
+                }
+        }
+        
+        ToolbarItem(placement: .principal) {
+            Text(cityName)
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+        }
+    }
 }
--- Invio-USGChallenge-2025/Features/CityMap/CityMapViewModel.swift
+++ Invio-USGChallenge-2025/Features/CityMap/CityMapViewModel.swift
@@ -7,13 +7,83 @@
 
 import Foundation
 import MapKit
+import CoreLocation
+import Combine
 
-class CityMapViewModel: ObservableObject {
+class CityMapViewModel: NSObject, ObservableObject {
+    // MARK: - Published properties
     @Published var cityName: String
     @Published var locations: [Location]
+    @Published var shouldShowSettingsAlert = false
+    @Published var userLocation: CLLocationCoordinate2D?
+    @Published var showLocationPermissionAlert = false
     
-    init(cityName: String, locations: [Location]) {
+    private var locationManager: LocationManager
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(
+        cityName: String,
+        locations: [Location],
+        locationManager: LocationManager = .shared
+    ) {
         self.cityName = cityName
         self.locations = locations
+        self.locationManager = locationManager
+        super.init()
+        bindLocationUpdates()
+    }
+    
+    // MARK: - Handle location permission state
+    func onAppear() {
+        if locationManager.authorizationStatus == .notDetermined {
+            showLocationPermissionAlert = true
+        } else if locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus == .authorizedAlways {
+            updateUserLocation()
+        } else if locationManager.authorizationStatus == .denied || locationManager.authorizationStatus == .restricted {
+            shouldShowSettingsAlert = true
+        }
+    }
+    
+    // MARK: - Handle location button behavior based on current permission state
+    func handleLocationButtonTap() -> Bool {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            updateUserLocation()
+            return true
+        case .notDetermined:
+            locationManager.requestLocationAccess()
+            return false
+        case .denied, .restricted:
+            locationManager.shouldShowSettingsAlert = true
+            return false
+        @unknown default:
+            return false
+        }
+    }
+    
+    // MARK: - Fetch current user location if authorized
+    func updateUserLocation() {
+        if locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus == .authorizedAlways {
+            locationManager.fetchUserLocation()
+        }
+    }
+    
+    // MARK: - Ask for location permission
+    func requestLocationPermission() {
+        locationManager.requestLocationAccess()
+    }
+
+    // MARK: - Open app settings if permission is denied
+    func openSettings() {
+        locationManager.openSettings()
+    }
+    
+    // MARK: - Bind location updates from LocationManager to the ViewModel
+    private func bindLocationUpdates() {
+        locationManager.$userLocation
+            .assign(to: &$userLocation)
+        locationManager.$shouldShowSettingsAlert
+            .assign(to: &$shouldShowSettingsAlert)
     }
 }
+
